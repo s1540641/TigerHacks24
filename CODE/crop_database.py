744164e5359397ac7751fd45c2c1b67db3678d10
s1540641 <magdlinem1305@gmail.com>
2024-11-02 11:50:51 -0500

    created crop database

--- CODE/crop_database.py
+++ CODE/crop_database.py
@@ -1,6 +1,20 @@
 # Logic for matching crops with user input
 
-class CropDatabase:
-    def __init__(self):
+backyard_crops_data = {
+    'cool_season': {
+        'well-drained': [
+            {'name':'lettuce', 'vitamins': ['A', 'C', 'K']},
+            {'name': 'Spinach', 'vitamins': ['A', 'C', 'K', 'B9']},
+            {'name': 'Carrots', 'vitamins': ['A', 'C', 'K', 'B6']},
+        ]
+    },
+   'warm_season': {
+        'muddy': [
+            {'name': 'okra', 'vitamins': ['A', 'C', 'K']},
+            {'name': 'eggplant', 'vitamins': ['B1', 'B6', 'K']},
+            {'name': 'peppers', 'vitamins': ['A', 'C', 'E']}
+        ]
+   },
+    
 
-def get_suitable_crops(self, month, temperature):
+}
