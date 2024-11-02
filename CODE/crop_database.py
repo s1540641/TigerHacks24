b0a5cdea2d8067ffc5d493ccbe641c1e8d2e6c56
s1540641 <magdlinem1305@gmail.com>
2024-11-02 12:14:39 -0500

    progress in crop_database...

--- CODE/crop_database.py
+++ CODE/crop_database.py
@@ -1,5 +1,6 @@
 # Logic for matching crops with user input
 
+#this is all the possible crop options that the user can get
 backyard_crops_data = {
     'cool_season': {
         'well-drained': [
@@ -9,12 +10,24 @@ backyard_crops_data = {
         ]
     },
    'warm_season': {
-        'muddy': [
+        'well-drained': [
             {'name': 'okra', 'vitamins': ['A', 'C', 'K']},
             {'name': 'eggplant', 'vitamins': ['B1', 'B6', 'K']},
             {'name': 'peppers', 'vitamins': ['A', 'C', 'E']}
         ]
    },
-    
-
+    'winter_season':{
+        'well-drained':[
+        {'name': 'kale', 'vitamins': ['A', 'C', 'K']},
+        {'name': 'brussels sprouts', 'vitamins': ['C', 'K', 'B6']},
+        {'name': 'leeks', 'vitamins': ['A', 'C', 'K']}
+        ]
+    },
 }
+#
+def match_crops(season, soil_condition):
+    try:
+        crops = backyard_crops_data[season][soil_condition]
+        return crops
+    except KeyError:
+        return "No crops available for the specified season and soil condition."
